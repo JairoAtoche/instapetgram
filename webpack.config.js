693a2462e3d924054556023b1c5fd01700ff8b27
JairoAtoche <jairoatoche90098@gmail.com>
2022-10-05 18:33:22 -0500

    add: estructura inicial para el proyecto de react agregando webpack, babel, eslint y prettier

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		/*
+    alias: {
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@routes": path.resolve(__dirname, "src/routes/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@utils": path.resolve(__dirname, "src/utils/"),
+    },
+    */
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: './public/index.html',
+			filename: 'index.html',
+			minify: {
+				collapseWhitespace: true,
+				keepClosingSlash: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true,
+			},
+		}),
+	],
+	devServer: {
+		static: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 3000,
+		open: true,
+	},
+};
